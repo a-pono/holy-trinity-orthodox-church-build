69730815b73c9998b9603dfa1ee4e3a03a12d9fb
Anatoliy Ponomarenko <anatoliy.e.ponomarenko@gmail.com>
2020-05-21 21:38:11 -0400

    Start working...

---
+++ precache-manifest.1ac14cdd02951f44e7b7ebbff37608ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42dc20bef020b606882d671d408122b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24a91e454a87d9cbc8d",
+    "url": "/static/css/main.6a7a50f1.chunk.css"
+  },
+  {
+    "revision": "99bd488fbb9a3f3303ee",
+    "url": "/static/js/2.9d7d61f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d7d61f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24a91e454a87d9cbc8d",
+    "url": "/static/js/main.525e16a5.chunk.js"
+  },
+  {
+    "revision": "fec60fc5c34ee501d074",
+    "url": "/static/js/runtime-main.6805960b.js"
+  },
+  {
+    "revision": "a36d756a59c9a3c349b2083635ddb093",
+    "url": "/static/media/logo_site.a36d756a.png"
+  }
+]);
